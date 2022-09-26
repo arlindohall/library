29197fe3e9ecdeb68d71e1714b92ef6d1d877343
Miller Arlindo Hall <hall@hey.com>
2022-09-25 22:32:58 -0400

    Add present validations to titles

--- app/models/title.rb
+++ app/models/title.rb
@@ -1,4 +1,6 @@
 class Title < ApplicationRecord
   belongs_to :author
   belongs_to :genre
+
+  validates :name, :author_id, :genre_id, presence: true
 end
--- test/models/title_test.rb
+++ test/models/title_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class TitleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @title = Title.new(
+      name: "The Dispossessed",
+      author_id: 1,
+      genre_id: 1,
+    )
+  end
+
+  test "genre is required" do
+    @title.genre_id = nil
+    assert_not @title.save
+  end
+
+  test "author is required" do
+    @title.author_id = nil
+    assert_not @title.save
+  end
+
+  test "name is required" do
+    @title.name = nil
+    assert_not @title.save
+  end
 end
